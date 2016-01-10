fff8e7f64ffd6050b351e0c62912d89b94965403
Alexander Nestorov <alexandernst@gmail.com>
2016-01-10 19:47:36 +0100

    Fix plugin interface

--- src/iface.h
+++ src/iface.h
@@ -1,16 +1,21 @@
+#ifndef _LARGEFILE64_SOURCE
+#define _LARGEFILE64_SOURCE
+#endif
+#define _FILE_OFFSET_BITS 64
+
 #ifndef IFACE_H_INCLUDED
 #define IFACE_H_INCLUDED
 
+using namespace std;
+
 typedef struct plugin_t {
 	void *hndl;
-	char *name;
-	char *description;
+	string name;
+	string description;
 } plugin_t;
 
-#ifdef __cplusplus
 #include "libs/cpp-bitstring/Bits.h"
 extern "C" plugin_t *init();
-extern "C" void process(unsigned char *mem_buf, unsigned long int mem_size);
-#endif
+extern "C" void process(Bits *data);
 
 #endif
